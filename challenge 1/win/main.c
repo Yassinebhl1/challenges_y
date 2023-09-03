f97b33524e2e1291e8e38423f8d8505b2be99cf6
Yassinebhl1 <bhlybhl@gmail.com>
2023-09-03 16:10:11 +0200

    done

---
+++ challenge 1/win/main.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char Nom;
+    char prenom;
+    int Age;
+    char Sex;
+    int num;
+
+
+    printf("ecrire votre Nom: ");
+    scanf("%s",&Nom);
+    printf("ecrire votre prenom: ");
+    scanf("%s",&prenom);
+    printf("ecrire votre Age: ");
+    scanf("%d",&Age);
+    printf("ecrire votre Sex: ");
+    scanf("%s",&Sex);
+     if(Sex='male'){
+        printf("DONT ENTER YOUR NUMBER");
+    }
+    else{
+        printf("you may enter your number please");
+        scanf("%d",&num);
+
+    }
+
+
+
+
+
+     return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
